0ceecfd81f5e2d829bb0bfb1ba8551d71b41143f
karenhuang925 <92420431+karenhuang925@users.noreply.github.com>
2022-08-30 22:01:49 -0700

    prop part 1

---
+++ practice-for-week-14-react-functional-components-props/src/BaseStats.js
@@ -0,0 +1,11 @@
+import './BaseStats.css'
+
+function baseStats({baseStats}) {
+    return (
+        <div className='base-stats'>
+            <h1>BaseStats</h1>
+        </div>
+    )
+}
+
+export default baseStats
